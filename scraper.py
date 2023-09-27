34fe7db1807435ff4d29591f9e56d209a90e1cd5
RomaTsybulia <romalonstejn@gmail.com>
2023-09-27 19:11:42 +0300

    Corrections has been made

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ with open("variables.json", "r") as json_file:
     TELEGRAM_BOT_TOKEN = data["telegram_bot_token"]
     chat_id = data["chat_id"]
     url = data["url"]
-    previous_advertisement_list = data["previous_advertisement_list"]
+    previous_advertisement = data["previous_advertisement"]
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
@@ -30,20 +30,18 @@ async def get_all_advertisements():
         advertisement.find('div', {'class': 'css-1jh69qu'})
         link_element = advertisement.find('a', class_='css-rc5s2u')
 
-        if link_element and not advertisement.find('div', {'class': 'css-1jh69qu'}):
+        if link_element and not advertisement.find('div',
+                                                   {'class': 'css-1jh69qu'}):
             link = "https://www.olx.ua/" + link_element['href']
             new_advertisement_list.append(link)
-    new_advertisement_list = list(reversed(new_advertisement_list))
 
-    for item in new_advertisement_list:
-        if item not in previous_advertisement_list:
-            message = f"Знайдено нові оголошення {url}"
-            await send_message_to_bot(chat_id=chat_id, message=message)
-            break
+    if previous_advertisement != new_advertisement_list[0]:
+        message = f"Знайдено нові оголошення {url}"
+        await send_message_to_bot(chat_id=chat_id, message=message)
 
     with open("variables.json", "w") as json_file:
-        data["previous_advertisement_list"] = new_advertisement_list
-        json.dump(data, json_file)
+        data["previous_advertisement"] = new_advertisement_list[0]
+        json.dump(data, json_file, indent=4)
 
 
 async def main():
